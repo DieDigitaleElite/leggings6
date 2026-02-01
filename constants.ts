66be4319b27ea1d17321a06097157e5008a8f2b4
DieDigitaleElite <orry@hotmail.de>
2026-02-01 16:44:26 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,18 +5,21 @@ export const MOCK_PRODUCTS: Product[] = [
   {
     id: 'set-sky-blue',
     name: 'Sky Blue Yoga Set',
+    price: '59,99 €',
     imageUrl: 'https://superbeautiful.de/thumbnail/39/d5/84/1688393421/produktfotoskyblue5_800x800.png',
     description: 'Dein sky-blue Set mit High-Neck Crop Top und perfekt sitzenden Leggings für maximale Bewegungsfreiheit.'
   },
   {
     id: 'set-maroon',
     name: 'Maroon Performance Set',
+    price: '64,95 €',
     imageUrl: 'https://superbeautiful.de/thumbnail/d1/a6/9f/1688394345/produktfotored1_800x800.png',
     description: 'Das exklusive Maroon Set kombiniert Style mit Performance. Atmungsaktiv und blickdicht.'
   },
   {
     id: 'set-black',
     name: 'Midnight Black Set',
+    price: '62,00 €',
     imageUrl: 'https://superbeautiful.de/thumbnail/b2/e7/77/1688394134/produktfotoblack6_800x800.png',
     description: 'Der Klassiker in Midnight Black. Zeitloses Design für jedes Workout und den Alltag.'
   }
@@ -25,6 +28,5 @@ export const MOCK_PRODUCTS: Product[] = [
 export const AVAILABLE_SIZES = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
 
 export const APP_CONFIG = {
-  TEXT_MODEL: 'gemini-3-flash-preview',
-  IMAGE_MODEL: 'gemini-2.5-flash-image'
+  MODEL_NAME: 'gemini-2.5-flash-image',
 };
